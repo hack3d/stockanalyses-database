e3b6ee9041c557b9b247b662cea171ff7565c44b
hack3d <hack3d.dev@googlemail.com>
2017-05-05 22:08:22 +0200

    add a check for a valid mysql-client

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# check if there is a valid mysql-client available
+if [ ! -z "$(which mysql)" ]; then
+	MYSQL="$(which mysql)"
+else
+	MYSQL="$(which mysqlaccess)"
+fi
+
 # import database at first
-mysql -h $3 -u $1 -p$2 < /opt/stockanalyses-database/stockanalyses_prod.sql 
+${MYSQL} -h ${3} -u ${1} -p${2} < /opt/stockanalyses-database/stockanalyses_prod.sql
 
 # create mysql user for every daemon
-mysql -h $3 -u $1 -p$2 < /opt/stockanalyses-database/create_user_stock-downloader.sql
-mysql -h $3 -u $1 -p$2 < /opt/stockanalyses-database/create_user_stock-importer.sql
+${MYSQL} -h ${3} -u ${1} -p${2} < /opt/stockanalyses-database/create_user_stock-downloader.sql
+${MYSQL} -h ${3} -u ${1} -p${2} < /opt/stockanalyses-database/create_user_stock-importer.sql
 
 # customize config files
 # stockanalyses-downloader
